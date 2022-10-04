317a437e6150da2e5c6c09a86ccf725b66aa71df
xuwenxiu2002 <1558017314@qq.com>
2022-10-04 09:38:12 +0000

    change

--- 02_built_in-types.dart
+++ 02_built_in-types.dart
@@ -3,13 +3,26 @@ void main(){
   var x = 18;
   var z = 1.1;
   num y = 18;
-
-  var one = '1';
-
   //自动转换
   double a = 1;
 
   //字符串和数字之间相互转换
   var one = int.parse('1');
+  var s2 = double.parse('2.1');
+  assert(s2 == 2.1);
+  print(('$s2'));
+
+  String oneAsString = 1.toString();
+  assert(oneAsString == '1');
+  print('$one');
+
+  String piAsString = 3.1415926.toStringAsFixed(3);
+  assert(piAsString == '3.142');
+  print('$piAsString');
+
+  //整型的位移：<< 左移，>> 右移
+  
+
+
 
 }
